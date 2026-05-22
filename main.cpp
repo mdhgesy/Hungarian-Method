2f8cb06ad337f7ff287454d20178d2a2bdb1ebd9
mdhgesy <mdhgesy@gmail.com>
2026-05-22 21:49:29 +0200

    Variable reading prototype

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+  int instances = 0;
+  scanf("%d", &instances);
+
+  for (int z = 0; z < instances; ++z)
+  {
+    int vertices = 0;
+    int edges = 0;
+    scanf("%d %d", &vertices, &edges);
+
+    for (int g = 0; g < edges; ++g)
+    {
+    }
+  }
+}
